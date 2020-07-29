19685a7c0257aead1de46114ab35d50d5764b48c
Supriya <supriyadhar2002@gmail.com>
2020-07-29 13:16:29 -0400

    B4 6 VC

---
+++ ViewController1.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController1.swift
+//  git hub connect
+//
+//  Created by Supriya Dhar on 7/29/20.
+//  Copyright © 2020 Supriya Dhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
